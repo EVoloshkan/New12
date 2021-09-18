c4ec77721b72eb77db469e74851a91b2804cd466
EVoloshkan <katerinka13001@gmail.com>
2021-09-18 15:48:13 +0300

    Update

--- src/Test.java
+++ src/Test.java
@@ -1,26 +1,30 @@
-import java.util.Locale;
-import java.util.Scanner;
-
 public class Test {
+
     public static void main(String[] args) {
         String a = "I like Java!!!";
+
         boolean contains = a.contains("Java");
+        System.out.println("Cодержит ли строка подстроку “Java” : " + contains);
+
         boolean startWith = a.startsWith("I like");
+        System.out.println("Начинается ли строка с подстроки “I like” : " + startWith);
+
         boolean endWith = a.endsWith("!!!");
-        String replaceA = a.replace('a', 'o');
-        System.out.println(contains);
-        System.out.println(startWith);
-        System.out.println(endWith);
+        System.out.println("Заканчивается ли строка с подставки “!!!” : " + endWith);
+
         if (contains && startWith && endWith) {
-            System.out.println(a.toUpperCase());
+            System.out.println("Предыдущие условия верны: " + a.toUpperCase());
         } else {
-            System.out.println("");
+            System.out.println("Условия не верны");
         }
+
+        String replaceA = a.replace('a', 'o');
         int index = replaceA.indexOf("Jovo");
         if (index == -1) {
             System.out.println("Не содержит Jovo");
         } else {
-            System.out.println(replaceA.substring(index, index + "Jovo".length()));
+            System.out.println("Подстрока " + replaceA.substring(index, index + "Jovo".length()));
         }
     }
+
 }
